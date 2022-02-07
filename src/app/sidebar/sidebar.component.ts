efeafb48e51bec43141eba80c026c77072c289f4
Nicolas Inostroza <ninostrozacordova2016@gmail.com>
2022-02-07 20:07:08 -0300

    Creando aplicacion de gifs

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  constructor(private gifsService: GifsService) { }
+
+  get historial(): string[] {
+    return this.gifsService.historial;
+  }
+
+
+
+
+
+}
